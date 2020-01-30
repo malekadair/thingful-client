7dea8e0a42b9270f58a8bf987fee720e10882044
Malek Haj-Hussein <malekadair@gmail.com>
2020-01-30 17:23:10 -0600

    first commit. Working on getting basic auth to work

--- src/contexts/ThingListContext.js
+++ src/contexts/ThingListContext.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const ThingListContext = React.createContext({
   thingList: [],
   error: null,
   setError: () => {},
   clearError: () => {},
-  setThingList: () => {},
-})
-export default ThingListContext
+  setThingList: () => {}
+});
+export default ThingListContext;
 
 export class ThingListProvider extends Component {
   state = {
     thingList: [],
-    error: null,
+    error: null
   };
 
   setThingList = thingList => {
-    this.setState({ thingList })
-  }
+    this.setState({ thingList });
+  };
 
   setError = error => {
-    console.error(error)
-    this.setState({ error })
-  }
+    console.error(error);
+    this.setState({ error });
+  };
 
   clearError = () => {
-    this.setState({ error: null })
-  }
+    this.setState({ error: null });
+  };
 
   render() {
     const value = {
@@ -34,12 +34,12 @@ export class ThingListProvider extends Component {
       error: this.state.error,
       setError: this.setError,
       clearError: this.clearError,
-      setThingList: this.setThingList,
-    }
+      setThingList: this.setThingList
+    };
     return (
       <ThingListContext.Provider value={value}>
         {this.props.children}
       </ThingListContext.Provider>
-    )
+    );
   }
 }
